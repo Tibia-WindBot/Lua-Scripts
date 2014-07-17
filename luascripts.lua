71147338e12151f734a66dde66f7f389e14b41c0
Leonardo Falk <leonardo.falk@hotmail.com>
2014-07-17 10:12:06 -0300

    Update luascripts.lua

--- luascripts.lua
+++ luascripts.lua
@@ -5,14 +5,13 @@ LUA_SCRIPTS = {
 		{link = "1536#post14771", name = "Amulet/Ring Equipper", author = "Raphael"},
 		{link = "239#post1809", name = "Relogger & Backpack Reopener", author = "Raphael"},
 		{link = "248#post1837", name = "OT Server Change Gold", author = "Raphael"},
-		{link = "1231#post11392", name = "Anti Kill Steal", author = "Leonardo"},
+		{link = "1231#post11392", name = "Anti Kill Steal (KS)", author = "Leonardo"},
 		{link = "157#post1084", name = "Auto Responder", author = "Leonardo"},
 		{link = "1837#post18117", name = "Kill Counter", author = "Raphael"},
 		{link = "151#post1049", name = "Spells Attacker", author = "Leonardo"},
 		{link = "1487#post14045", name = "Loot Sorter (Ground)", author = "SuNe"},
-		{link = "1204#post11169", name = "Sio/Mass Res/Healer", author = "jakub"},
+		{link = "1204#post11169", name = "Sio/Mass Res/Spell Healer", author = "jakub"},
 		{link = "10501#post9625", name = "Anti Furniture", author = "Leonardo"},
-		{link = "1546#post14894", name = "Screenshot on Death", author = "tryller"},
 		{link = "812#post6804", name = "Dice Gambling", author = "xyyyyllo"},
 		{link = "696#post5505", name = "Anti Stalker", author = "Raphael"},
 		{link = "1537#post14774", name = "Food Eater", author = "Raphael"},
@@ -31,6 +30,23 @@ LUA_SCRIPTS = {
 		{link = "697#post5516", name = "Anti Idle", author = "Raphael"},
 		{link = "408#post2994", name = "Flask Dropper", author = "Raphael"},
 		{link = "25#post142", name = "Loot Logger", author = "Raphael"},
+		{link = "5371#post59984", name = "Withdraw Items from DP", author = "Tanochi"},
+		{link = "6274#post70339", name = "PK Guild Alert", author = "Raphael"},
+		{link = "2531#post26031", name = "Safe Reconnect", author = "Leonardo"},
+		{link = "2512#post25864", name = "Navigation SD Combo", author = "Raphael"},
+		{link = "4123#post45823", name = "Ultimate Ring Equipper", author = "FaNtA"},
+		{link = "2506#post25810", name = "Character Relogger on Low Stamina", author = "Raphael"},
+		{link = "2508#post25833", name = "Smart(?) Healing", author = "Raphael"},
+		{link = "2352#post24123", name = "ML Trainer with Supply Buyer", author = "borges"},
+		{link = "5671#post63538", name = "Selling Loot in Shop", author = "jesusz0r"},
+		{link = "5593#post62575", name = "Go back to hunting if went up/downstairs", author = "jesusz0r"},
+		{link = "2364#post24283", name = "Area Rune Shooter", author = "Leonardo"},
+		{link = "2077#post20701", name = "Screenshot on Advance and Death", author = "Leonardo"},
+		{link = "3354#post35732", name = "Anti Push", author = "mistgun"},
+		{link = "2319#post23625", name = "Safe Stamina", author = "Hultin"},
+		{link = "4923#post54383", name = "Remote Info", author = "rukasu"},
+		{link = "4347#post47807", name = "Market Seller", author = "ragekemi"},
+		{link = "2345#post24058", name = "Basic Ring Equipper", author = "xyyyyllo"},
 	},
 
 	HUDS = {
@@ -48,11 +64,14 @@ LUA_SCRIPTS = {
 		{link = "845#post7071", name = "Life/Mana Percent", author = "eachziN"},
 		{link = "729#post5808", name = "Hotkeys", author = "Turc"},
 		{link = "547#post4100", name = "Waypoints Buttons", author = "Forgee"},
+		{link = "6318#post70963", name = "Show Runes", author = "Leonardo"},
+		{link = "2266#post23028", name = "Hultin's HUD", author = "Hultin"},
+		{link = "3018#post31311", name = "Advanced Player on Screen", author = "Colandus"},
+		{link = "3660#post40108", name = "Tile Item Spy", author = "xyyyyllo"},
+		{link = "3064#post31778", name = "Flash Client HP/Mana Bar", author = "darklighty"},
+		{link = "2019#post20052", name = "Soft Boots Duration", author = "Donatello"},
 	},
 }
-
--- Filtering Entries
--- no need filtering actually
 
 local PATH_PATTERN = 'http://forums.tibiawindbot.com/showthread.php?%s'
 
@@ -71,3 +90,61 @@ end
 
 table.sort(LUA_SCRIPTS.PERSISTENTS, sortFunction)
 table.sort(LUA_SCRIPTS.HUDS, sortFunction)
+
+local HEADER_TOP = [[
+[CENTER][B][SIZE=7]List of Scripts and HUD's[/SIZE][/B][/CENTER]
+
+[hr][/hr]
+
+[CENTER][B][SIZE=5]Hotkeys and Persistents[/SIZE][/B][/CENTER]
+
+[TABLE="width: 768, align: center, class: cms_table_sortable"]
+[tr="bgcolor: #000000"]
+	[td="width: 50%%"][B][I][COLOR="#FFFFFF"]Name[/I][/B]:[/COLOR][/td]
+	[td="width: 25%%"][I][B][COLOR="#FFFFFF"]Author[/B][/I]:[/COLOR][/td]
+	[td="width: 25%%"][B][I][COLOR="#FFFFFF"]Link[/I][/B]:[/COLOR][/td]
+[/tr]
+%s
+[/TABLE]
+]]
+
+local LINE_PATTERN = [[
+[tr="bgcolor: %s"]
+	[td="width: 50%%"]%s[/td]
+	[td="width: 25%%"]%s[/td]
+	[td="width: 25%%"][URL=%s]Link[/URL][/td]
+[/tr]
+]]
+
+local HEADER_BOTTOM = [[
+
+[hr][/hr]
+
+[CENTER][B][SIZE=5]Heads Up Displays (HUD's)[/SIZE][/B][/CENTER]
+
+[TABLE="width: 768, align: center, class: cms_table_sortable"]
+[tr="bgcolor: #000000"]
+	[td="width: 50%%"][B][I][COLOR="#FFFFFF"]Name[/I][/B]:[/COLOR][/td]
+	[td="width: 25%%"][I][B][COLOR="#FFFFFF"]Author[/B][/I]:[/COLOR][/td]
+	[td="width: 25%%"][B][I][COLOR="#FFFFFF"]Link[/I][/B]:[/COLOR][/td]
+[/tr]
+%s
+[/TABLE]
+]]
+
+function createlist(tbl)
+	local str = ''
+
+	for i, item in ipairs(tbl) do
+		str = str .. string.format(LINE_PATTERN:sub(1), i % 2 == 0 and '#cccccc' or '#ffffff', item.name, item.author, string.format(PATH_PATTERN, item.link))
+	end
+
+	return str:sub(1, -2)
+end
+
+local fl = io.open("../output.txt", "w+")
+if (fl ~= nil) then
+	fl:write(string.format(HEADER_TOP, createlist(LUA_SCRIPTS.PERSISTENTS)):sub(1, -2) .. string.format(HEADER_BOTTOM, createlist(LUA_SCRIPTS.HUDS)):sub(1, -2))
+	fl:flush()
+	fl:close()
+end
